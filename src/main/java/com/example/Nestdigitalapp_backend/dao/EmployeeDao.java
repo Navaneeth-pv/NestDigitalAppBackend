f76b815fddadfec11615e79c25dbf45b46415344
Navaneeth-pv <navaneethpv902@gmail.com>
2022-12-12 17:45:46 +0530

    employee bug fix

--- src/main/java/com/example/Nestdigitalapp_backend/dao/EmployeeDao.java
+++ src/main/java/com/example/Nestdigitalapp_backend/dao/EmployeeDao.java
@@ -1,7 +1,21 @@
 package com.example.Nestdigitalapp_backend.dao;
 
 import com.example.Nestdigitalapp_backend.model.Employee;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface EmployeeDao extends CrudRepository<Employee,Integer> {
+
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM `employee` WHERE `id` =:id",nativeQuery = true)
+    void deleteEmployee(@Param("id") Integer id);
+
+    @Query(value = "SELECT `id`, `empcode`, `empdesignation`, `empname`, `emppassword`, `empusername` FROM `employee` WHERE `empcode` LIKE %:empcode%",nativeQuery = true)
+    List<Employee> SearchEmployee(@Param("empcode") String empcode);
 }
