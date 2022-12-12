19c2b2cd40a7d0f66cf9b7689fae97c1da25ef5f
Navaneeth-pv <navaneethpv902@gmail.com>
2022-12-12 11:58:01 +0530

    first commit

---
+++ src/test/java/com/example/Nestdigitalapp_backend/NestdigitalappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Nestdigitalapp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NestdigitalappBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
